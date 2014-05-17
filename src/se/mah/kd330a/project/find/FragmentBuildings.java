3ceadba0f7b3105248e80d1c4d8e82c52dfe05a8
cwshearer <coppnorm@gmail.com>
2014-05-17 15:33:41 +0200

    added two new classes to replace FragmentFind.java and FragmentBuilding.java, as well as layouts to replace fragment_screen_find.xml and fragment_screen_find_building.xml

---
+++ src/se/mah/kd330a/project/find/FragmentBuildings.java
@@ -0,0 +1,169 @@
+package se.mah.kd330a.project.find;
+
+import java.io.InputStream;
+import java.util.Locale;
+
+import se.mah.kd330a.project.R;
+import se.mah.kd330a.project.faq.FragmentFaq;
+import se.mah.kd330a.project.find.data.RoomDbHandler;
+import se.mah.kd330a.project.find.view.FragmentBuilding;
+import se.mah.kd330a.project.find.view.FragmentFloorMap;
+import se.mah.kd330a.project.find.view.FragmentMaps;
+import se.mah.kd330a.project.find.view.FragmentResult;
+import se.mah.kd330a.project.help.FragmentCredits;
+import se.mah.kd330a.project.home.FragmentHome;
+import se.mah.kd330a.project.itsl.FragmentITSL;
+import se.mah.kd330a.project.schedule.view.FragmentScheduleWeekPager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.TextView.OnEditorActionListener;
+
+
+public class FragmentBuildings extends Fragment implements OnClickListener {
+
+	//creating objects for each item in the buildings list
+	private LinearLayout house1;
+	private LinearLayout house2;
+	private LinearLayout house3;
+	private LinearLayout house4;
+	private LinearLayout house5;
+	private LinearLayout house6;
+	private LinearLayout house7;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		//inflating layout
+		ViewGroup rootView = (ViewGroup) inflater
+				.inflate(R.layout.fragment_screen_find_buildings, container, false);
+		
+		//Initializing the UI components by linking the objects to their view and setting click listener        
+        house1 = (LinearLayout) rootView.findViewById(R.id.house1);
+        house1.setOnClickListener(this);
+        
+        house2 = (LinearLayout) rootView.findViewById(R.id.house2);
+        house2.setOnClickListener(this);
+        
+        house3 = (LinearLayout) rootView.findViewById(R.id.house3);
+        house3.setOnClickListener(this);
+
+        house4 = (LinearLayout) rootView.findViewById(R.id.house4);
+        house4.setOnClickListener(this);
+
+        house5 = (LinearLayout) rootView.findViewById(R.id.house5);
+        house5.setOnClickListener(this);
+
+        house6 = (LinearLayout) rootView.findViewById(R.id.house6);
+        house6.setOnClickListener(this);
+        
+        house7 = (LinearLayout) rootView.findViewById(R.id.house7);
+        house7.setOnClickListener(this);
+        
+		return rootView;
+	}
+	
+	//needed to produce an option item in the menu bar (google maps icon)
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+	
+	//creating the option item in the menu bar (google maps icon)
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+	    // TODO Add your menu entries here
+	    super.onCreateOptionsMenu(menu, inflater);
+	    inflater.inflate(R.menu.find_all, menu);
+	}
+	
+	//setting an action to what happens when the option item is clicked
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.find_menu_google_all:
+			//opening a kml file (google maps file) from res/raw that contains the pre-marked positions of all buildings
+			Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=android.resource://res/raw/mah_buildings"));
+			startActivity(i);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	//handles clicks
+	@Override
+	public void onClick(View v) { 
+
+		//calls on the method that loads a building's GUI depending on which item in the list was clicked
+		if(v.getId() == R.id.house1){
+			selectBuilding(1);
+		}else if(v.getId() == R.id.house2){
+			selectBuilding(2);
+		}else if(v.getId() == R.id.house3){
+			selectBuilding(3);
+		}else if(v.getId() == R.id.house4){
+			selectBuilding(4);
+		}else if(v.getId() == R.id.house5){
+			selectBuilding(5);
+		}else if(v.getId() == R.id.house6){
+			selectBuilding(6);
+		}else if(v.getId() == R.id.house7){
+			selectBuilding(7);
+		}
+		
+	}
+	
+	//gets the building code form a position in an array and puts it as the parameter in the method that loads a fragment with the respective building's GUI
+	private void selectBuilding(int pos) {
+		String buildingCode;
+		Resources res = getResources();
+		String[] findCode = res
+				.getStringArray(R.array.find_building_code_array);
+		buildingCode = findCode[pos];
+		showBuildingMap(buildingCode);
+	}
+	
+	//loads the fragment with the building maps
+	private void showBuildingMap(String buildingCode) {
+		Fragment fragment = new FragmentMaps();
+		Bundle args = new Bundle();
+		args.putString(FragmentMaps.ARG_BUILDING, buildingCode);
+		fragment.setArguments(args);
+
+		FragmentManager	 fragmentManager = getActivity().getSupportFragmentManager();
+		FragmentTransaction fragmentTrans = fragmentManager.beginTransaction();	
+		fragmentTrans.replace(R.id.content_frame, fragment);
+		fragmentTrans.addToBackStack(null);
+		fragmentTrans.commit();		
+	}
+	
+}
---
+++ src/se/mah/kd330a/project/find/view/FragmentMaps.java
@@ -0,0 +1,147 @@
+package se.mah.kd330a.project.find.view;
+
+import java.util.Arrays;
+
+import se.mah.kd330a.project.R;
+import se.mah.kd330a.project.find.data.GetImage;
+import se.mah.kd330a.project.find.data.ImageLoader;
+import se.mah.kd330a.project.find.data.ImageLoader.OnImageLoaderListener;
+import se.mah.kd330a.project.find.view.FragmentFloorMap.MyJavaScriptInterface;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+//THIS ENTIRE FRAGMENT IS A COPY OF FragmentBuilding.java  ******************************************************************************
+//THE IDEA IS THAT THIS CLASS WILL LOAD THE SWIPE-VIEW LAYOUT AND CLASSES
+
+public class FragmentMaps extends Fragment implements OnImageLoaderListener {
+	public static final String ARG_BUILDING = "building";
+	private String buildingCode;
+	
+	private ImageView imgNav;
+	private ProgressBar prgBar;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		ViewGroup rootView = (ViewGroup) inflater
+				.inflate(R.layout.fragment_screen_find_building_maps, container, false);
+		imgNav = (ImageView) rootView.findViewById(R.id.img_find_navigation);
+		imgNav.setVisibility(View.INVISIBLE);
+		prgBar = (ProgressBar) rootView.findViewById(R.id.pb_find_loading);
+		prgBar.setVisibility(View.VISIBLE);
+		
+		return rootView;
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Bundle args = getArguments();
+		
+		buildingCode = args.getString(ARG_BUILDING);
+		String[] buildings = getResources().getStringArray(R.array.find_building_code_array);
+		final int pos = Arrays.asList(buildings).indexOf(buildingCode);
+		
+		buildings = getResources().getStringArray(R.array.find_building_array);
+		((TextView) getView().findViewById(R.id.text_find_startTitle)).setText(buildings[pos]);
+		
+		Log.i("project", "FragmentBuilding " + buildingCode);
+		
+		new ImageLoader(getActivity(), this).execute("b" + buildingCode + ".jpg");
+		
+		LinearLayout llFloor = (LinearLayout) getView().findViewById(R.id.ll_find_floormap);
+		llFloor.setClickable(true);
+		llFloor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Fragment fragment = new FragmentFloorMap();
+				Bundle args = new Bundle();
+				args.putString(FragmentBuilding.ARG_BUILDING, buildingCode);
+				fragment.setArguments(args);
+
+				FragmentManager	 fragmentManager = getActivity().getSupportFragmentManager();
+
+				FragmentTransaction fragmentTrans = fragmentManager.beginTransaction();	
+				fragmentTrans.replace(R.id.content_frame, fragment);
+				fragmentTrans.addToBackStack(null);
+				fragmentTrans.commit();
+			}
+		});
+	
+		
+		//VARIABLES AND onClick-METHOD FOR LINKING OUT TO GOOGLE MAPS--------------------------------------------------------------------------------
+		llFloor = (LinearLayout) getView().findViewById(R.id.ll_find_google);
+		llFloor.setClickable(true);
+		llFloor.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String[] buildingNames = getResources().getStringArray(R.array.find_building_array);
+				String location = buildingNames[pos];
+
+				if(location.equals("Klerken (Kl)")){
+					Log.i("gmaps","klerken");
+					location = "Carl Gustafs vag 34";
+				}else if(location.equals("University Hospital (As)")){
+					Log.i("gmaps","university hospital");
+					location = "Jan Waldenstroms gata 25";
+				}else if(location.equals("Kranen (K2)") || location.equals("Ubåtshallen (K8)")){
+					Log.i("gmaps","kranen & ubåten");
+					location = "Östra Varvsgatan 11 A";
+				}else if(location.equals("Orkanen (Or)")){
+					Log.i("gmaps","orkanen");
+					location = "Nordenskiöldsgatan 10";
+				}else if(location.equals("Gäddan (G8)")){
+					Log.i("gmaps","gäddan");
+					location = "Citadellsvägen 7";
+				}
+
+				//getting the google map
+				Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+						Uri.parse("geo:0,0?q="+location+"+Malmo+Sweden"));
+						Log.i("test","clicked ll_find_google");
+				startActivity(i);
+			}});
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.find, menu);
+	}
+	
+	@Override
+	public void onImageReceived(String fileName) {
+		prgBar.setVisibility(View.INVISIBLE);
+		imgNav.setImageBitmap(GetImage.getImageFromLocalStorage(fileName, getActivity()));
+		imgNav.setVisibility(View.VISIBLE);	
+	}
+}
